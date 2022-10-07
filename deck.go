2ac5ea043194e5e7cd66399ba9e31525d280bbdd
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-10-07 15:14:46 +0600

    Initial commit

---
+++ deck.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// Which is a slice of strings
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuit := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValue := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuit {
+		for _, value := range cardValue {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
